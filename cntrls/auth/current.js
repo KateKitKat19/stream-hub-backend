9a4c06754615004b0b81c351a6cb9a801e59aae0
Kate Dziubchuk <103567509+KateKitKat19@users.noreply.github.com>
2023-06-28 12:48:33 +0300

    correct user data returned

--- cntrls/auth/current.js
+++ cntrls/auth/current.js
@@ -2,13 +2,16 @@ const { User } = require("../../models/UserModel");
 const { HttpError, errorCatcher } = require("../../helpers");
 
 const getCurrent = async (req, res, next) => {
-  const token = req.body;
+    const { token } = req.body;
+    if (!token) {
+      next(HttpError(401));
+    }
   const isCurrent = await User.findOne({ token });
 
   if (isCurrent) {
     res.status(201).json({
       status: 201,
-      user: { name: name, email: newUser.email },
+      user: { name: isCurrent.name, email: isCurrent.email },
       token: token,
     });
   } else {
